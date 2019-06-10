cee93410b587ad447cea47edb208a4f7d082913e
Camilo <camilo.jorquera.a@gmail.com>
2019-06-10 09:30:27 -0400

    Initial commit

---
+++ src/main/kotlin/cl/obj/tddjvm/Main.kt
@@ -0,0 +1,11 @@
+package cl.obj.tddjvm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TddForApproachApplication
+
+fun main(args: Array<String>) {
+	runApplication<TddForApproachApplication>(*args)
+}
